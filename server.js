e967d96d9d3d1e2b5b670e3dec54ddadcf37ad58
Mariana Pires Curti <marianapirescurti@MacBook-Pro.lan>
2024-10-04 02:23:32 -0400

    added init function

--- server.js
+++ server.js
@@ -1,7 +1,36 @@
+// import for express and pg
 const express = require('express');
 const app = express();
 const pg = require('pg');
 const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/acme_departments_employees_db');
 const port = process.env.PORT || 3000;
 
+// APP ROUTES
+// parse body into JS objects
 app.use(express.json())
+// log requests as they come in
+
+// Read 
+// Create 
+// Update
+// Delete
+
+// init fucntion
+const init = async () => {
+  await client.connect();
+  console.log('connected to database');
+  let SQL = `
+  
+  `;
+  await client.query(SQL);
+  console.log('tables created');
+  SQL = `
+  
+  `;
+  await client.query(SQL);
+  console.log('data seeded');
+  app.listen(port, () => console.log(`listening on port ${port}`));
+}
+
+// init invoke
+init ();
